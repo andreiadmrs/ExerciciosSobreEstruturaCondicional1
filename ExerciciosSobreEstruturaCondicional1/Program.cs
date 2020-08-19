bb89168391cf03190fb1d1d7d4b4f3dd1f8fef21
Andreia de Moraes <andreiadmrs@gmail.com>
2020-08-19 00:12:59 -0300

    exercicios do curso da udemy csharp

---
+++ ExerciciosSobreEstruturaCondicional1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExerciciosSobreEstruturaCondicional1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.*/
+
+            Console.WriteLine("Digite um número:");
+            int n1 = int.Parse(Console.ReadLine());
+            
+
+            if (n1 >= 0)
+            {
+                Console.WriteLine("Número positivo");
+            }
+            else
+            {
+                Console.WriteLine("Número negativo");
+            }
+        }
+    }
+}
